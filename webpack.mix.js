f538e4335c573e3a9743db1e2013fbdc4409e5a3
Moe Sbaiti <moesbaiti@hotmail.com>
2023-03-20 15:04:53 -0400

    La page d'accueil a été complétée en incluant la structure pour poursuivre le côté réactif de chaque section. Des fonctionnalités telles que les animations sont encore en cours d'élaboration.

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,10 +11,9 @@ const mix = require("laravel-mix");
  |
  */
 
-mix.js("resources/js/app.js", "public/js").postCss(
-    "resources/css/app.css",
-    "public/css",
-    [require("tailwindcss"), require("autoprefixer")]
-);
+mix.js("resources/js/app.js", "public/js");
+mix.js("resources/js/custom.js", "public/js");
+
+mix.postCss("resources/css/app.css", "public/css");
 
 mix.sass("resources/sass/app.scss", "public/css");
