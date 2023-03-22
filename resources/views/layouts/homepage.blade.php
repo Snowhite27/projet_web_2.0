773a82d81efaa4c09417fb95c7f25bd8a55723b6
Olivier Moreau <moreau.olivier@outlook.com>
2023-03-22 10:36:33 -0400

    Modification SCSS accueil de 'mix' vers 'asset'
    Modification du SCSS de la page d'accueil de 'mix' vers 'asset' & restauration du style de tailwind dans la section admin

--- resources/views/layouts/homepage.blade.php
+++ resources/views/layouts/homepage.blade.php
@@ -6,7 +6,7 @@
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>@yield('title')</title>
       <link rel="stylesheet" href="https://use.typekit.net/vko1nol.css%22%3E">
-      <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+      <link rel="stylesheet" href="{{ asset('css/index/style.css') }}">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
       <head>
    <link rel="shortcut icon" href="{{ asset('/images/arttech_imgs/favicon/favicon_32x32.png') }}">
