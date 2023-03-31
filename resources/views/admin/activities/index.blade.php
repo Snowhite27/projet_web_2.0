2a9c6ba75a8899bcf5ed0523e02fc6db754c8fcb
Olivier Moreau <moreau.olivier@outlook.com>
2023-03-31 13:37:30 -0400

    Modification style admin/activities, dark mode
    Ajout du dark theme pour les pages d'affichage, modification et d'ajout

--- resources/views/admin/activities/index.blade.php
+++ resources/views/admin/activities/index.blade.php
@@ -1,10 +1,8 @@
 <x-app-layout>
     <div class="pt-10">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex justify-center">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg w-[50%]">
-                <div class="p-6 bg-white border-b border-gray-200 flex flex-col justify-center items-center">
-                    <a href="{{ route('admin.activities.add') }}">Ajouter une activité</a>
-                </div>
+            <div class="border border-[#181a1b] hover:border-[#f9b96f] bg-[#181a1b] text-[#e5f0ff] overflow-hidden shadow-sm sm:rounded-lg w-[25%]">
+                <a class="p-6 flex flex-col justify-center items-center" href="{{ route('admin.activities.add') }}">Ajouter une activité</a>
             </div>
         </div>
     </div>
@@ -12,25 +10,25 @@
     @foreach ($activities as $activity)
     <div class="pt-10">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg flex flex-row justify-between lg:px-8 h-fit">
+            <div class="bg-[#181a1b] text-[#e5f0ff] overflow-hidden shadow-sm sm:rounded-lg flex flex-row justify-between lg:px-8 h-fit">
                 <div class="flex flex-row max-w-[75%]">
                     <div class="aspect-square h-full flex flex-col items-start justify-center items-center">
                         <img class="object-contain h-48 p-4" src="{{ asset('images/activities/' . ($activity->picture)) }}" alt="">
                     </div>
-                    <div class="p-6 border-b border-gray-200 flex flex-col justify-evenly items-start">
+                    <div class="p-6 flex flex-col justify-evenly items-start">
                         <p class="font-extrabold">{{ $activity->name }}</p>
                         <p class="font-extralight">{{ $activity->description }}</p>
                     </div>
                 </div>
                 <div class="flex flex-col justify-evenly items-center w-[50px]">
-                    <a class="bg-blue-500 w-full aspect-square rounded-lg flex justify-center items-center" href="{{ route('admin.activities.edit', $activity->id ) }}">
+                    <a class="hover:border hover:border-[#f9b96f] bg-[#1e2022] w-full aspect-square rounded-lg flex justify-center items-center" href="{{ route('admin.activities.edit', $activity->id ) }}">
                         <img class="w-8" src="{{ asset('icons/edit.png')}}" alt="">
                     </a>
-                    <form class="bg-red-500 w-full aspect-square rounded-lg flex justify-center items-center" action="{{ route('admin.activities.remove', $activity->id) }}" method="POST">
+                    <form class="hover:border hover:border-[#f9b96f] bg-[#1e2022] w-full aspect-square rounded-lg flex justify-center items-center" action="{{ route('admin.activities.remove', $activity->id) }}" method="POST">
                         @csrf
                         @method('DELETE')
-                        <img class="w-8" src="{{ asset('icons/remove.png')}}" alt="">
-                        <input type="submit" value="" class="cursor-pointer">
+                        <img class="absolute w-8" src="{{ asset('icons/remove.png')}}" alt="">
+                        <input class="absolute w-12 cursor-pointer aspect-square" type="submit" value="" class="cursor-pointer">
                     </form>
                 </div>
             </div>
