fb247386431207638867f5ffb6ce9e9f8deb8f6f
Moe Sbaiti <moesbaiti@hotmail.com>
2023-04-05 01:02:00 -0400

    Contenu complet de la page d'accueil en place. Les animations pour les sections qui contiennent des éléments spécifiques sur la page d'accueil fonctionnent avec un script JS personnalisé créé. Les boutons ont également été intégrés avec des ajustements de style spécifiques.

--- resources/views/layouts/homepage.blade.php
+++ resources/views/layouts/homepage.blade.php
@@ -24,6 +24,7 @@
    <button class="scroll-top"> <i class="fa-solid fa-chevron-up"></i></button>
    <body>
       @yield('content')
+      <script src="{{ asset('js/anim-on-scroll.js') }}"></script>
       <script src="{{ asset('js/scroll-top.js') }}"></script>
       <script src="{{ asset('js/custom.js') }}"></script>
       <script src="{{ asset('js/toggle_nav.js') }}"></script>
